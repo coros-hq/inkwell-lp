8d07dbf6e371ad4c68c4fff8fa75d4fa30ced6e6
oussamaalyapay <oussama@alyapay.com>
2026-09-23 17:52:46 +0000

    pumping version

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = "0.8.2";
+export const APP_VERSION = "0.8.4";
 export const GITHUB_URL = "https://github.com/coros-hq/inkwell";
 export const RELEASE_URL = `${GITHUB_URL}/releases/latest`;
